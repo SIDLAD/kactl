384b17646f64b8bf6c332b438603c57561a108c0
Siddharth <f20210438@pilani.bits-pilani.ac.in>
2024-12-22 02:39:21 +0530

    p commit

--- content/data-structures/DynamicPersistentSegTree.h
+++ content/data-structures/DynamicPersistentSegTree.h
@@ -1,22 +1,20 @@
-/**
- * Description: tree.construct(arr) to build a segment tree on arr.
- * tree.update(l, r, value, version) to update the segment tree.
- * tree.query(0, n-1, l, r, version_ptr) to query the segment tree.
- * 
- */
-#pragma once
+
 
 class DynamicPersistentSegTree{
+public:
     class Node{
     public:
         Node *left = nullptr;
         Node *right = nullptr;
         int val = 0, prop = 0;
     };
+
     int n = 0;
     bool create_version_on_update = false;
+    vector<Node *> ptrs{};
     vector<int> vec{};
-    Node *create_child(Node *&curr){
+
+    Node *newKid(Node *&curr){
         auto newNode = new Node();
         newNode->left = curr->left;
         newNode->right = curr->right;
@@ -24,18 +22,19 @@ class DynamicPersistentSegTree{
         newNode->val = curr->val;
         return newNode;
     }
+
     void lazy(int i, int j, Node *curr){
-        if (!curr->prop)
-            return;
+        if (!curr->prop) return;
         curr->val = (j - i + 1) * curr->prop;
         if (i != j){
-            curr->left = create_child(curr->left);
-            curr->right = create_child(curr->right);
+            curr->left = newKid(curr->left);
+            curr->right = newKid(curr->right);
             curr->left->prop = curr->prop;
             curr->right->prop = curr->prop;
         }
         curr->prop = 0;
     }
+
     Node *construct(int i, int j){
         auto newNode = new Node();
         if (i == j){
@@ -43,19 +42,21 @@ class DynamicPersistentSegTree{
         }
         else{
             int mid = i + (j - i) / 2;
-            auto left = construct(i, mid);
+            auto leftt = construct(i, mid);
             auto right = construct(mid + 1, j);
-            newNode->val = left->val + right->val;
-            newNode->left = left;
+            newNode->val = leftt->val + right->val;
+            newNode->left = leftt;
             newNode->right = right;
         }
         return newNode;
     }
+
     Node *update(int i, int j, int l, int r, int value, Node *curr){
         lazy(i, j, curr);
         if (i >= l && j <= r){
-            Node *newNode = create_child(curr);
+            Node *newNode = newKid(curr);
             newNode->prop += value;
+            lazy(i, j, newNode);
             return newNode;
         }
         if (i > r || j < l){
@@ -68,9 +69,8 @@ class DynamicPersistentSegTree{
         newNode->val = newNode->left->val + newNode->right->val;
         return newNode;
     }
-    int query(int i, int j, int l, int r, Node *curr = nullptr){
-        if (curr == nullptr)
-            curr = ptrs.back();
+
+    int query(int i, int j, int l, int r, Node *curr){
         lazy(i, j, curr);
         if (j < l || r < i){
             return 0;
@@ -79,32 +79,30 @@ class DynamicPersistentSegTree{
             return curr->val;
         }
         int mid = i + (j - i) / 2;
-        return query(i, mid, l, r, curr->left) + query(mid + 1, j, l, r, curr->right);
+        return query(i, mid, l, r, curr->left)+query(mid + 1, j, l, r, curr->right);
     }
-public:
-    vector<Node *> ptrs{};
-    int query(int i, int j, int version=-1){
-        if (version == -1) version = ptrs.size() - 1;
-        return query(0, n - 1, i, j, ptrs[version]);
-    }
+
     void construct(const vector<int> &vec){
-        if (vec.empty())
+        if (vec.empty()){
             return;
+        }
         n = vec.size();
         this->vec = vec;
         auto root = construct(0, n - 1);
         ptrs.push_back(root);
     }
+
     void update(int l, int r, int value, int version = -1){
-        if (version == -1)
-            version = ptrs.size() - 1;
-        if (create_version_on_update)
-            ptrs.push_back(update(0, n - 1, l, r, value, ptrs[version]));
-        else
-            ptrs[version] = update(0, n - 1, l, r, value, ptrs[version]);
+        if (version == -1) version = ptrs.size() - 1;
+        ptrs[version] = update(0, n - 1, l, r, value, ptrs[version]);
     }
-    int copy_version(int version) { 
-        ptrs.push_back(create_child(ptrs[version])); 
-        return ptrs.size()-1;
+
+    void copy_version(int version){
+        ptrs.push_back(newKid(ptrs[version]));
+    }
+
+    int query(int l, int r, int version = -1){
+        if (version == -1) version = ptrs.size() - 1;
+        return query(0, n - 1, l, r, ptrs[version]);
     }
 };
